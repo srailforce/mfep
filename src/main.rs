d1af41a94325a9465ef6862e714669fa57619b03
fnlcon <fnlcon@outlook.com>
2023-04-25 20:50:13 +0800

    imports

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,23 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
-use axum::Router;
-use http::{Request, Response, HeaderName, HeaderValue};
-use hyper::{Body, server::conn::AddrStream};
+use http::{HeaderValue};
+use hyper::{server::conn::AddrStream};
 use hyper::service::make_service_fn;
 use mfep::{load_config, service::responsor::AutoResponsor};
-use axum::{Server, ServiceExt};
-use tower::{Service, service_fn, ServiceBuilder};
+use axum::{Server};
+use tower::{ServiceBuilder};
 use tower_http::set_header::SetResponseHeaderLayer;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()>{
     let config = load_config("config.yaml")?;
 
-    let make_svc = make_service_fn(|socket: &AddrStream| {
-        let remote_addr = socket.remote_addr();
+    let make_svc = make_service_fn(|_: &AddrStream| {
         async move {
             let svc = AutoResponsor::new();
             let stack = ServiceBuilder::new()
-            .layer(tower_http::set_header::SetResponseHeaderLayer::overriding(http::header::ACCESS_CONTROL_ALLOW_ORIGIN, HeaderValue::from_static("*")))
+            .layer(SetResponseHeaderLayer::overriding(http::header::ACCESS_CONTROL_ALLOW_ORIGIN, HeaderValue::from_static("*")))
             .service(svc);
             Ok::<_, Infallible>(stack)
         }
